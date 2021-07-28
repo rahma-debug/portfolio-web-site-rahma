bcb7bbb7aa235b75d757e56dac3b20a4cea391cb
rahma-debug <rahma.jallouli@enis.tn>
2021-07-28 17:15:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const hearts = Array.from(document.getElementsByClassName("like"));
+function handleLike(event) {
+event.target.classList.toggle("red-heart");
+}
+
+for (let i = 0; i < hearts.length; i++) {
+hearts[i].addEventListener("click", handleLike);
+}
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
